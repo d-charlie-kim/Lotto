e5f0c61355f193c926c6cde5972ebc3aaaf7654c
d-charlie-kim <d.charlie.kim@gmail.com>
2023-05-08 03:11:38 +0900

    [Feat] update Lotto count and print properly

--- src/js/Display.js
+++ src/js/Display.js
@@ -1,5 +1,9 @@
-class Display {
-	
+const $lottoCount = document.querySelector('.lotto-count');
+
+const Display = {
+	updateLottoCount(count) {
+		$lottoCount.textContent = `총 ${count}개를 구매하였습니다.`
+	},
 }
 
 export default Display;
