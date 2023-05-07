56151bb79aedd06214b481026d11e8b9194b163b
d-charlie-kim <d.charlie.kim@gmail.com>
2023-05-08 03:12:55 +0900

    [Feat] buy lotto function & make random six number function

--- src/js/App.js
+++ src/js/App.js
@@ -1,34 +1,44 @@
 import Lotto from './Lotto.js';
 import Result from './Result.js';
 import Validator from './Validator.js';
-const validator = new Validator();
+import Display from './Display.js';
 
 class App {
-	#money;
+	#totalMoney;
+	#lottoCount;
 	#lottos;
 
 	constructor() {
-		this.#money = 0;
+		this.#totalMoney = 0;
+		this.#lottoCount = 0;
 		this.#lottos = [];
 	}
 
 	// 처음 금액을 입력 받으면 play
 	// 1000원 단위인지 체크 후 lotto 사러 가기
 	play(money) {
-		if (!validator.isValidMoney(money))
+		if (!Validator.isValidMoney(money))
 			alert('금액을 1,000원 단위로 입력해주세요.');
 		else {
-			this.#money = money;
-			console.log(this.#money);
+			this.#totalMoney += money;
+			this.buyLottos(money / 1000);
 		}
 	}
-
+	
 	// 로또 사기
-	buyLotto(count) {
-		while (count > 0) {
+	// 배열에 lotto 개수별로 채우기
+	buyLottos(count) {
+		this.#lottoCount += count;
+
+		for (let i = 0; i < count; i++) {
 			this.#lottos.push(new Lotto());
-			count--;
 		}
+
+		Display.updateLottoCount(this.#lottoCount);
+		//NOTE 로또 잘 샀는지 확인
+		// for (let i = 0; i < this.#lottos.length; i++) {
+		// 	console.log(this.#lottos[i].getLotto());
+		// }
 	}
 }
 
--- src/js/Lotto.js
+++ src/js/Lotto.js
@@ -3,9 +3,26 @@ import Validator from './Validator.js';
 class Lotto {
 	#numbers;
 
-	constructor(numbers) {
-		Validator.validate(numbers);
-		this.#numbers = numbers;
+	constructor() {
+		this.#numbers = this.randomSixNum();
+	}
+
+	// Math.random 활용 6개 숫자 뽑기
+	// 오름차순으로 정렬해서 #numbers에 넣어준다.
+	randomSixNum() {
+		let lotto = [];
+		for (let i = 0; i < 6; ) {
+			let newNum = Math.floor(Math.random() * 45);
+			if (newNum === 0 || lotto.includes(newNum))
+				continue;
+			lotto.push(newNum);
+			i++;
+		}
+		return(lotto.sort((a,b) => (a - b)));
+	}
+
+	getLotto() {
+		return this.#numbers;
 	}
 }
 
