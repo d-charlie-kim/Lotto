3b79a33c36eacbef7636e181a281b2504b91689e
d-charlie-kim <d.charlie.kim@gmail.com>
2023-05-08 16:33:09 +0900

    [Feat] 결과 모달창 출력

--- src/js/Display.js
+++ src/js/Display.js
@@ -4,6 +4,27 @@ const Display = {
 	updateLottoCount(count) {
 		$lottoCount.textContent = `총 ${count}개를 구매하였습니다.`
 	},
+
+	printLottoList(lottos) {
+
+	},
+
+	deleteLottoList() {
+
+	},
+
+	printResult(result) {
+		const $lottoResultScreen = document.querySelectorAll('.lotto-result');
+		const $lottoResultRate = document.querySelector('.lotto-result-rate');
+		const matchCount = result.getResult();
+		const lose = matchCount[0] + matchCount[1] + matchCount[2];
+
+		$lottoResultScreen[0].textContent = `${lose}개`;
+		for (let i = 3; i < matchCount.length; i++) {
+				$lottoResultScreen[i - 2].textContent = `${matchCount[i]}개`;
+		}
+		$lottoResultRate.textContent = `당신의 총 수익률은 ${result.getRate()}%입니다.`
+	}
 }
 
 export default Display;
--- src/js/Result.js
+++ src/js/Result.js
@@ -1,5 +1,85 @@
 class Result {
-	
+	#winningNum;
+	#bonusNum;
+	#rate;
+	#matchCount;
+	#prize;
+
+	constructor(winning, bonus) {
+		this.#winningNum = winning;
+		this.#bonusNum = bonus;
+		this.#rate = 0;
+		this.#matchCount = new Array(8).fill(0);
+		this.#prize = 0;
+	}
+
+	calculateRate(lottoCount) {
+		const consume = lottoCount * 1000;
+		const earn = this.#prize;
+		this.#rate = ((earn - consume) / consume).toFixed(2);
+	}
+
+	calculatePrize() {
+		for (let i = 3; i < this.#matchCount.length; i++) {
+			switch(i) {
+				case 3:
+					this.#prize += (this.#matchCount[i] * 5000);
+					continue;
+				case 4:
+					this.#prize += (this.#matchCount[i] * 50000);
+					continue;
+				case 5:
+					this.#prize += (this.#matchCount[i] * 1500000);
+					continue;
+				case 6:
+					this.#prize += (this.#matchCount[i] * 30000000);
+					continue;
+				case 7:
+					this.#prize += (this.#matchCount[i] * 2000000000);
+					continue;
+			}
+		}
+	}
+
+	putWinningArray(count, bonus) {
+		if (count === 6)
+			this.#matchCount[7]++;
+		else if (count === 5 && bonus)
+			this.#matchCount[6]++;
+		else
+			this.#matchCount[count]++;
+	}
+
+	checkWinning(lottos) {
+		lottos.forEach((el) => {
+			const lotto = el.getLotto();
+			let bonus = false;
+			if (lotto.includes(this.#bonusNum))
+				bonus = true;
+
+			let count = 0;
+			lotto.forEach((num) => {
+				if (this.#winningNum.includes(num))
+					count++;
+			});
+
+			this.putWinningArray(count, bonus);
+		})
+		this.calculatePrize();
+		this.calculateRate(lottos.length);
+	}
+
+	getPrize() {
+		return this.#prize;
+	}
+
+	getResult() {
+		return this.#matchCount;
+	}
+
+	getRate() {
+		return this.#rate;
+	}
 }
 
 export default Result;
