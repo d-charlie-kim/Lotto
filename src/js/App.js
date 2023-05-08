80604587496cde8617b823df886eb1b6ad394cb3
d-charlie-kim <d.charlie.kim@gmail.com>
2023-05-08 16:34:08 +0900

    [Feat] 결과 모달창 출력

--- src/js/App.js
+++ src/js/App.js
@@ -4,12 +4,10 @@ import Validator from './Validator.js';
 import Display from './Display.js';
 
 class App {
-	#totalMoney;
 	#lottoCount;
 	#lottos;
 
 	constructor() {
-		this.#totalMoney = 0;
 		this.#lottoCount = 0;
 		this.#lottos = [];
 	}
@@ -20,7 +18,6 @@ class App {
 		if (!Validator.isValidMoney(money))
 			alert('금액을 1,000원 단위로 입력해주세요.');
 		else {
-			this.#totalMoney += money;
 			this.buyLottos(money / 1000);
 		}
 	}
@@ -36,9 +33,36 @@ class App {
 
 		Display.updateLottoCount(this.#lottoCount);
 		//NOTE 로또 잘 샀는지 확인
-		// for (let i = 0; i < this.#lottos.length; i++) {
-		// 	console.log(this.#lottos[i].getLotto());
-		// }
+		for (let i = 0; i < this.#lottos.length; i++) {
+			console.log(this.#lottos[i].getLotto());
+		}
+	}
+
+	// 번호보기 토글 클릭 시 로또 번호들 출력
+	printLottoList() {
+		Display.printLottoList(this.#lottos);
+	}
+
+	// 번호보기 토글 다시 클릭 시 로또 번호들 사라짐
+	deleteLottoList() {
+		Display.deleteLottoList();
+	}
+
+	// 결과 확인하기 버튼 클릭 시 모달창에 뜰 결과 만들기
+	// 우선 입력된 당첨 번호 및 보너스 번호가 유효한지 체크
+	getResult() {
+		const $winningNumber = document.querySelectorAll('.winning-number');
+		const $bonusNumber = document.querySelector('.bonus-number');
+		const winning = [];
+		const bonus = parseInt($bonusNumber.value);
+		$winningNumber.forEach((el) => winning.push(parseInt(el.value)));
+
+		if (!Validator.isValidWinningNum(winning, bonus))
+			return ;
+
+		const result = new Result(winning, bonus);
+		result.checkWinning(this.#lottos);
+		Display.printResult(result);
 	}
 }
 
